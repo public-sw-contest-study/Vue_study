70dec7ad472946235765d400057c52840085f7b6
kosw <swko4512@gmail.com>
2023-06-06 14:22:34 +0900

    vue method event

--- main/src/App.vue
+++ main/src/App.vue
@@ -5,25 +5,54 @@
     <h2>{{ animals }}</h2>
     <h2 v-for="(animal,index) in animals" :key="index">
     <span v-if="animal !== 'rat'">{{ animal }} index : {{ index }}</span></h2>
+
+    <h1>{{ cnt + 1 }}</h1>
+    <button v-on:click="addNumber(1)">up</button>
+    <h1>{{ cnt + 1 }}</h1>
+    <button v-on:click="minNumber">down</button>
+    <h1>{{ cnt + 1 }}</h1>
+    <button v-on:mouseover="addNumber(10)">10up</button>
+
+
+
+    <h2>{{ message }}</h2>
+    <button @click="greeting">hi</button>
+    <button @click="grreeting">bye</button>
+    <div @click="getmousepo" class="box"></div>
+
+    <button @click="addNumber($event,10)">10plus</button><button @click="addNumber($event,100)">100plus</button>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'App',
+  methods: {
+    getmousepo(event){
+      console.log(event);
+      this.message = `마우스의 위치는 ${event.pageX},${event.pageY}`;
+    },
+    greeting(){
+      this.message = "hihi"
+    },
+    grreeting(){
+      this.message = "byebye"
+    },
+    addNumber(event,value){
+      this.message = `${event.pageX} adsa ${value}asd${event.pageX+value}`;
+    },
+    minNumber(){
+      this.cnt = this.cnt - 1;
+    },
+},
   data(){
     return {
-      age: 15,
-      display: false,
-      animals: ["monkey","rat","dog","lion"],
-      users: [
-        {name: 'scalper', job: 'developer', gender:'male'},
+      cnt: 0,
+      message: ""
 
-        {name: 'jon', job: 'developer', gender:'male'},
-
-        {name: 'asd', job: 'developer', gender:'male'},
-      ]
     };
+
   }
 }
 </script>
@@ -36,5 +65,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  width: 100px;
+  height: 100px;
+  font-size: 24px;
+}
+.box{
+  width: 300px;
+  height: 300px;
+  background: #2c3e50;
 }
 </style>
